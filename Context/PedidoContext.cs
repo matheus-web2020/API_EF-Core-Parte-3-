61a3e068438896cced4795d0baa921fd70352b48
Matheus <matheus.fusca.lima@outlook.com>
2020-10-02 09:57:51 -0300

    Falta Comentar

---
+++ Context/PedidoContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF_Core_API.Context
+{
+    public class PedidoContext
+    {
+    }
+}
